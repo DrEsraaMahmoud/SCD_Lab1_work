e9c954fa0546e169065ca7bd50fc0fcbf3d3d95a
esraa <esra_a_2008@live.com>
2023-03-10 17:36:38 +0300

    First_commit

---
+++ test.java
@@ -0,0 +1,12 @@
+Public class Hello{
+
+   Public static void main(String argv[]){
+          String name"World";
+          if(argv.length !=0){
+            name = argv[0];
+    }
+
+    System.out.printLn("Hello, " + name + " !");
+}
+
+}
